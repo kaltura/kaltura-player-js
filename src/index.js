1abc17f639f089cb54ee86d56f64f79c26fdd8ea
Oren Me <oren.me@gmail.com>
2017-06-13 07:51:08 +0300

    :sunrise: first commit
    Just run build or start scripts to start hacking for now

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Provider from 'playkit-js-providers/dist/ovpProvider';
+import * as Playkit from 'playkit-js';
+import PlaykitUI from 'playkit-js-ui/dist/playkit-js-ui';
+import '../node_modules/playkit-js-ui/src/styles/style.css';
+import 'playkit-js-hls';
+import 'playkit-js-dash';
+
+export {Playkit, Provider, PlaykitUI};
+
