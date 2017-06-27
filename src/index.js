a2d0879ba47df9ace8d7e353c9eb2227c5b54f96
Dvir Hazut <dvir.hazut@kaltura.com>
2017-06-27 10:04:25 +0200

    Adding player tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
-import Provider from 'playkit-js-providers/dist/ovpProvider';
 import * as Playkit from 'playkit-js';
-import PlaykitUI from 'playkit-js-ui/dist/playkit-js-ui';
+import {UIManager} from '../node_modules/playkit-js-ui/src/playkit-js-ui';
 import '../node_modules/playkit-js-ui/src/styles/style.css';
-import 'playkit-js-hls';
-import 'playkit-js-dash';
+import DashAdapter from 'playkit-js-dash';
+import HlsAdapter from 'playkit-js-hls';
 
-export {Playkit, Provider, PlaykitUI};
+// Register DashAdapter to the media source adapter manager
+if (DashAdapter.isSupported()) {
+  Playkit.registerMediaSourceAdapter(DashAdapter);
+}
+
+// Register hls adapter to the media source adapter provider.
+if (HlsAdapter.isSupported()) {
+  Playkit.registerMediaSourceAdapter(HlsAdapter);
+}
+
+let configs = [];
+var players = [];
+var uis = [];
+
+configs[0] = {
+  "target": "player1",
+  "sources": [
+    {
+      "mimetype": "application/x-mpegurl",
+      "url": "https://wowzaec2demo.streamlock.net/vod-multitrack/_definst_/smil:ElephantsDream/ElephantsDream.smil/playlist.m3u8"
+    }
+  ]
+};
+
+configs[1] = {
+  "target": "player2",
+  "sources": [
+    {
+      "mimetype": "application/dash+xml",
+      "url": "https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd"
+    }
+  ]
+};
+
+
+configs.forEach((c, i) => {
+  players[i] = Playkit.playkit(c);
+  uis[i] = new UIManager(players[i], c);
+  uis[i].buildDefaultUI();
+});
+
+export {Playkit};
 
