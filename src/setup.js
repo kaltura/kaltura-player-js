9f954fa71cce7929a28519aa635263e81f0cba40
Dan Ziv <dan.ziv@kaltura.com>
2017-06-27 23:55:49 +0300

    sync changes from playkit-js

--- src/setup.js
+++ src/setup.js
@@ -4,18 +4,19 @@ import OvpProvider from 'playkit-js-providers/dist/ovpProvider';
 import handleSessionId from './session-id'
 
 /**
- *
- * @param {Object} config - contains partnerId and entryID
- * @return {Promise<*>} - player promise
+ * Setup the kaltura player.
+ * @param {string} targetId - The target id of the dom element which we append the player to.
+ * @param {Object} config - Fully config which includes partnerId and entryId.
+ * @return {Promise<*>} - The player promise.
  */
-export default function setup(config: Object): Promise<*> {
-  let player = Playkit.playkit();
+export default function setup(targetId: string, config: Object): Promise<*> {
+  let player = Playkit.loadPlayer(targetId);
   player.addEventListener(player.Event.SOURCE_SELECTED, (event) => {
     handleSessionId(event.payload.selectedSource, player);
   });
   if (config) {
     let provider = new OvpProvider(config.partnerId);
-    return provider.getConfig(config.entryID)
+    return provider.getConfig(config.entryId)
       .then(data => {
         player.configure(data);
         return Promise.resolve(player);
