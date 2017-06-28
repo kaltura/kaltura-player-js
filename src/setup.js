0e47824ddcd4466d7978729b5f6439564581a814
Dan Ziv <dan.ziv@kaltura.com>
2017-06-28 11:57:49 +0300

    return provider from setup

--- src/setup.js
+++ src/setup.js
@@ -4,25 +4,32 @@ import OvpProvider from 'playkit-js-providers/dist/ovpProvider';
 import handleSessionId from './session-id'
 
 /**
- *
- * @param {Object} config - contains partnerId and entryID
- * @return {Promise<*>} - player promise
+ * Setup the kaltura player.
+ * @param {string} targetId - The target id of the dom element which we append the player to.
+ * @param {Object} config - Fully config which includes partnerId and entryId.
+ * @return {Promise<*>} - The player promise.
  */
-export default function setup(config: Object): Promise<*> {
-  let player = Playkit.playkit();
-  player.addEventListener(player.Event.SOURCE_SELECTED, (event) => {
-    handleSessionId(event.payload.selectedSource, player);
+export default function setup(targetId: string, config: Object): Promise<*> {
+  let response = {};
+  return new Promise((resolve, reject) => {
+    response.player = Playkit.loadPlayer(targetId);
+    response.player.addEventListener(response.player.Event.SOURCE_SELECTED, (event) => {
+      handleSessionId(event.payload.selectedSource, response.player);
+    });
+    if (config) {
+      if (config.partnerId) {
+        response.provider = new OvpProvider(config.partnerId);
+        if (config.entryId) {
+          return response.provider.getConfig(config.entryId)
+            .then(data => {
+              response.player.configure(data);
+              resolve(response);
+            }).catch(error => {
+              reject(error);
+            });
+        }
+      }
+    }
+    resolve(response);
   });
-  if (config) {
-    let provider = new OvpProvider(config.partnerId);
-    return provider.getConfig(config.entryID)
-      .then(data => {
-        player.configure(data);
-        return Promise.resolve(player);
-      }).catch(error => {
-        return Promise.reject(error);
-      });
-  } else {
-    return Promise.resolve(player);
-  }
 }
