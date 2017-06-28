b12d1a2da5df156f722d2d7ac04dcbd3d4ab2516
Dan Ziv <dan.ziv@kaltura.com>
2017-06-28 21:40:38 +0300

    merge handling of setup response

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 // @flow
 
 // Import core
-import * as Playkit from 'playkit-js';
+import * as Playkit from 'playkit-js'
 
 // Import OVP and OTT providers
-import OvpProvider from 'playkit-js-providers/dist/ovpProvider';
+import OvpProvider from 'playkit-js-providers/dist/ovpProvider'
 
 // Import UI
-import PlaykitUI from 'playkit-js-ui';
-import '../node_modules/playkit-js-ui/src/styles/style.css';
+import PlaykitUI from 'playkit-js-ui'
+import '../node_modules/playkit-js-ui/src/styles/style.css'
 
 // Import media source adapters
 import 'playkit-js-hls';
 import 'playkit-js-dash';
 
+// Import setup method
 import setup from './setup'
+
 // TODO: Import plugins
 
 export {Playkit, OvpProvider, PlaykitUI, setup};
