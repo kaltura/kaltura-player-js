168891bb0cabf60c8df6087fb3e36975dc16a5d0
Oren Me <oren.me@gmail.com>
2017-06-28 18:36:21 +0300

    chore: handle player and UI bootstrapping to DOM

--- src/setup.js
+++ src/setup.js
@@ -1,8 +1,10 @@
 //@flow
 import * as Playkit from 'playkit-js';
+import PlaykitUI from 'playkit-js-ui';
 import OvpProvider from 'playkit-js-providers/dist/ovpProvider';
 import handleSessionId from './session-id'
 
+const CONTAINER_CLASS_NAME: string = 'kalturaplayer-container';
 /**
  * Setup the kaltura player.
  * @param {string} targetId - The target id of the dom element which we append the player to.
@@ -10,7 +12,10 @@ import handleSessionId from './session-id'
  * @return {Promise<*>} - The player promise.
  */
 export default function setup(targetId: string, config: Object): Promise<*> {
-  let player = Playkit.loadPlayer(targetId);
+  let el = createKalturaPlayerContainer(targetId);
+  let player = Playkit.loadPlayer(el.id);
+  let playerUIManager = new PlaykitUI(player, Object.assign({}, config, {targetId: el.id}));
+  playerUIManager.buildDefaultUI();
   player.addEventListener(player.Event.SOURCE_SELECTED, (event) => {
     handleSessionId(event.payload.selectedSource, player);
   });
@@ -26,4 +31,20 @@ export default function setup(targetId: string, config: Object): Promise<*> {
   } else {
     return Promise.resolve(player);
   }
+}
+
+function createKalturaPlayerContainer(targetId: string){
+  let el = document.createElement("div");
+  el.id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  el.className = CONTAINER_CLASS_NAME;
+  el.setAttribute('tabindex', '-1');
+  el.style.width = "100%";
+  el.style.height= "100%";
+  el.style.position = "relative";
+
+  let parentNode = document.getElementById(targetId);
+  if ((parentNode != null) && (el != null)) {
+    parentNode.appendChild(el);
+  }
+  return el;
 }
