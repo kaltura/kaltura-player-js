efc2e07544c7b2341186557d1df1c71e0924937a
Dan Ziv <dan.ziv@kaltura.com>
2017-06-28 12:38:10 +0300

    fix build

--- src/setup.js
+++ src/setup.js
@@ -6,28 +6,26 @@ import handleSessionId from './session-id'
 /**
  * Setup the kaltura player.
  * @param {string} targetId - The target id of the dom element which we append the player to.
- * @param {Object} config - Fully config which includes partnerId and entryId.
+ * @param {Object} providerConfig - Optional config which includes partnerId and an optional entryId.
  * @return {Promise<*>} - The player promise.
  */
-export default function setup(targetId: string, config: Object): Promise<*> {
+export default function setup(targetId: string, providerConfig: ?Object): Promise<*> {
   let response = {};
   return new Promise((resolve, reject) => {
     response.player = Playkit.loadPlayer(targetId);
     response.player.addEventListener(response.player.Event.SOURCE_SELECTED, (event) => {
       handleSessionId(event.payload.selectedSource, response.player);
     });
-    if (config) {
-      if (config.partnerId) {
-        response.provider = new OvpProvider(config.partnerId);
-        if (config.entryId) {
-          return response.provider.getConfig(config.entryId)
-            .then(data => {
-              response.player.configure(data);
-              resolve(response);
-            }).catch(error => {
-              reject(error);
-            });
-        }
+    if (providerConfig && providerConfig.partnerId) {
+      response.provider = new OvpProvider(providerConfig.partnerId);
+      if (providerConfig.entryId) {
+        return response.provider.getConfig(providerConfig.entryId)
+          .then(playerConfig => {
+            response.player.configure(playerConfig);
+            resolve(response);
+          }).catch(error => {
+            reject(error);
+          });
       }
     }
     resolve(response);
--- test/src/setup.spec.js
+++ test/src/setup.spec.js
@@ -1,42 +1,60 @@
 import setup from '../../src/setup'
 import * as TestUtils from 'playkit-js/test/src/utils/test-utils'
 
+const targetId = 'player-placeholder_setup.spec';
+
 describe('setup', function () {
 
-  let config, player;
+  let providerConfig, player, provider;
+
+  before(function () {
+    TestUtils.createElement('DIV', targetId);
+  });
 
   beforeEach(function () {
-    config = {
-      partnerId: 2196781, entryID: '1_h14v9eug'
+    providerConfig = {
+      partnerId: 2196781,
+      entryId: '1_h14v9eug'
     };
   });
 
   afterEach(function () {
     player.destroy();
+    provider = null;
     TestUtils.removeVideoElementsFromTestPage();
   });
 
+  after(function () {
+    TestUtils.removeElemenet(targetId);
+  });
+
   it('should create a full player', function (done) {
-    setup(config).then(p => {
-      player = p;
-      player.config.id.should.equal(config.entryID);
-      player.config.session.partnerID.should.equal(config.partnerId);
+    setup(targetId, providerConfig).then(response => {
+      player = response.player;
+      provider = response.provider;
+      provider.should.exist;
+      provider.partnerID.should.equal(providerConfig.partnerId);
+      player.config.id.should.equal(providerConfig.entryId);
+      player.config.session.partnerID.should.equal(providerConfig.partnerId);
       done();
     });
   });
 
   it('should create an empty player', function (done) {
-    setup().then(p => {
-      player = p;
+    setup(targetId).then(response => {
+      player = response.player;
       (!player.config.id).should.be.true;
       done();
     });
   });
 
   it('should decorate the selected source by session id', function (done) {
-    setup(config).then(p => {
-      player = p;
+    setup(targetId, providerConfig).then(response => {
+      player = response.player;
       player.load();
+      provider = response.provider;
+      provider.should.exist;
+      provider.partnerID.should.equal(providerConfig.partnerId);
       let sessionIdRegex = /playSessionId=((?:[a-z0-9]|-|:)*)/i;
       player.config.session.id.should.exist;
       sessionIdRegex.exec(player.src)[1].should.equal(player.config.session.id);
