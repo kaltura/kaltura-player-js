b42a7ed768afb34d22ed4d23ecdfcdc6bd1b07e4
Dan Ziv <dan.ziv@kaltura.com>
2017-09-07 15:58:42 +0300

    tests

---
+++ test/src/storage/storage-wrapper.spec.js
@@ -0,0 +1,47 @@
+import StorageWrapper from '../../../src/storage/storage-wrapper'
+
+describe('StorageWrapper', function () {
+  let storage;
+  let prefix = 'testPrefix.';
+
+  beforeEach(function () {
+    storage = new StorageWrapper(prefix);
+  });
+
+  afterEach(function () {
+    storage = null;
+    window.localStorage.clear();
+  });
+
+  it('should have size of 0', function () {
+    storage.size.should.equal(0);
+  });
+
+  it('should set an item in the local storage', function () {
+    if (storage._isLocalStorageAvailable) {
+      let key = 'test';
+      storage.setItem(key, 1);
+      storage.size.should.equal(1);
+      window.localStorage[prefix + key].should.exist;
+      window.localStorage[prefix + key].should.equal('1');
+    }
+  });
+
+  it('should get an item from local storage', function () {
+    if (storage._isLocalStorageAvailable) {
+      let key = 'test';
+      storage.setItem(key, 2);
+      storage.size.should.equal(1);
+      let value = storage.getItem(key);
+      value.should.equal(2);
+    }
+  });
+
+  it('should validate a key correctly', function (done) {
+    try {
+      storage._validateKey(1);
+    } catch (e) {
+      done();
+    }
+  });
+});
