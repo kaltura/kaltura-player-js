969586b6e51045ee3b2bef3b4d7b0938e1d2e871
Dan Ziv <dan.ziv@kaltura.com>
2017-09-24 13:56:10 +0300

    Fix tests

--- test/src/storage/storage-wrapper.spec.js
+++ test/src/storage/storage-wrapper.spec.js
@@ -1,45 +1,41 @@
 import StorageWrapper from '../../../src/storage/storage-wrapper'
+import {name} from '../../../package.json'
+
+const STORAGE_PREFIX = name + '_';
 
 describe('StorageWrapper', function () {
-  let storage;
-  let prefix = 'testPrefix.';
-
-  beforeEach(function () {
-    storage = new StorageWrapper(prefix);
-  });
 
   afterEach(function () {
-    storage = null;
     window.localStorage.clear();
   });
 
   it('should have size of 0', function () {
-    storage.size.should.equal(0);
+    StorageWrapper.size.should.equal(0);
   });
 
   it('should set an item in the local storage', function () {
-    if (storage._isLocalStorageAvailable) {
+    if (StorageWrapper._isLocalStorageAvailable) {
       let key = 'test';
-      storage.setItem(key, 1);
-      storage.size.should.equal(1);
-      window.localStorage[prefix + key].should.exist;
-      window.localStorage[prefix + key].should.equal('1');
+      StorageWrapper.setItem(key, 1);
+      StorageWrapper.size.should.equal(1);
+      window.localStorage[STORAGE_PREFIX + key].should.exist;
+      window.localStorage[STORAGE_PREFIX + key].should.equal('1');
     }
   });
 
   it('should get an item from the local storage', function () {
-    if (storage._isLocalStorageAvailable) {
+    if (StorageWrapper._isLocalStorageAvailable) {
       let key = 'test';
-      storage.setItem(key, 2);
-      storage.size.should.equal(1);
-      let value = storage.getItem(key);
+      StorageWrapper.setItem(key, 2);
+      StorageWrapper.size.should.equal(1);
+      let value = StorageWrapper.getItem(key);
       value.should.equal(2);
     }
   });
 
   it('should validate a wrong key', function (done) {
     try {
-      storage.setItem(2, 2);
+      StorageWrapper.setItem(2, 2);
     } catch (e) {
       done();
     }
