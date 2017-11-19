b5ab5c58b5db13c9f1cb3779dd6795b875568f01
Dan Ziv <dan.ziv@kaltura.com>
2017-11-19 21:49:36 +0200

    Update setup-helpers.js

--- src/utils/setup-helpers.js
+++ src/utils/setup-helpers.js
@@ -262,7 +262,7 @@ function setLogLevel(options: Object): void {
 function getUrlParameter(name: string) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
   const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-  const results = regex.exec(location.search);
+  let results = regex.exec(location.search);
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
 
