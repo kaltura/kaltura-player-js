b2797251a1adc034f0c04a4caa6df4c184dfdd65
Dan Ziv <dan.ziv@kaltura.com>
2018-06-25 13:43:26 +0300

    chore: include kava in the player bundle (#133)
    Temporary include kava plugin in the player bundle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 // @flow
 import PolyfillManager from './common/polyfills/polyfill-manager'
 import './common/polyfills/all'
-import getLogger, {getLogLevel, setLogLevel, LogLevel} from './common/utils/logger'
+import getLogger, {getLogLevel, LogLevel, setLogLevel} from './common/utils/logger'
+// Import core
+import * as core from 'playkit-js'
+// Import ui
+import * as ui from 'playkit-js-ui'
+// Import provider
+import * as providers from 'playkit-js-providers'
+// Import media source adapters
+import 'playkit-js-hls'
+import 'playkit-js-dash'
+// Import analytics plugin
+import 'playkit-js-analytics'
+import 'playkit-js-kava'
+// Import shaka-player
+import * as shaka from 'shaka-player'
+// Import setup method
+import {setup} from './setup'
 
 declare var __VERSION__: string;
 declare var __NAME__: string;
@@ -15,29 +31,6 @@ getLogger().log(`%c For more details see ${__PACKAGE_URL__}`, "color: #ff98f9;")
 setLogLevel(currentLogLevel);
 
 PolyfillManager.installAll();
-
-// Import core
-import * as core from 'playkit-js'
-
-// Import ui
-import * as ui from 'playkit-js-ui'
-
-// Import provider
-import * as providers from 'playkit-js-providers'
-
-// Import media source adapters
-import 'playkit-js-hls'
-import 'playkit-js-dash'
-
-
-// Import analytics plugin
-import 'playkit-js-analytics'
-
-// Import shaka-player
-import * as shaka from 'shaka-player'
-
-// Import setup method
-import {setup} from './setup'
 
 export {
   core,
