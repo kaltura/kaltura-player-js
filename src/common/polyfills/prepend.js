0766cae765d350fe861fe3b3bd823538e79a029f
Dan Ziv <dan.ziv@kaltura.com>
2018-09-12 11:27:18 +0300

    remove function closure

--- src/common/polyfills/prepend.js
+++ src/common/polyfills/prepend.js
@@ -7,30 +7,28 @@ export default class PrependPolyfill {
   static _logger: any = getLogger('PrependPolyfill');
 
   static install(): void {
-    (function(arr) {
-      arr.forEach(function(item) {
-        if (item.hasOwnProperty('prepend')) {
-          PrependPolyfill._logger.debug('No need to install polyfill on item', item);         
-          return;
+    [Element.prototype, Document.prototype, DocumentFragment.prototype].forEach(function(item) {
+      if (item.hasOwnProperty('prepend')) {
+        PrependPolyfill._logger.debug('No need to install polyfill on item', item);
+        return;
+      }
+      Object.defineProperty(item, 'prepend', {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: function prepend() {
+          var argArr = Array.prototype.slice.call(arguments),
+            docFrag = document.createDocumentFragment();
+
+          argArr.forEach(function(argItem) {
+            var isNode = argItem instanceof Node;
+            docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+          });
+
+          this.insertBefore(docFrag, this.firstChild);
         }
-        Object.defineProperty(item, 'prepend', {
-          configurable: true,
-          enumerable: true,
-          writable: true,
-          value: function prepend() {
-            var argArr = Array.prototype.slice.call(arguments),
-              docFrag = document.createDocumentFragment();
-
-            argArr.forEach(function(argItem) {
-              var isNode = argItem instanceof Node;
-              docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
-            });
-
-            this.insertBefore(docFrag, this.firstChild);
-          }
-        });
       });
-    })([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+    });
   }
 }
 
