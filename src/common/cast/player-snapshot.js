d413c2d765536aa8714e69ad11699220c73ff279
Dan Ziv <dan.ziv@kaltura.com>
2018-10-08 12:37:32 +0300

    Cast to live with correct current time

--- src/common/cast/player-snapshot.js
+++ src/common/cast/player-snapshot.js
@@ -13,13 +13,30 @@ class PlayerSnapshot {
 
   constructor(player: KalturaPlayer) {
     const activeTracks = player.getActiveTracks();
-    this.startTime = player.isLive() && !player.isDvr() ? 0 : player.currentTime;
+    this.startTime = getStartTime(player);
     this.autoplay = player.currentTime === 0 ? true : !player.paused;
     this.textStyle = player.textStyle;
     this.mediaInfo = player.getMediaInfo();
     this.audioLanguage = activeTracks.audio && activeTracks.audio.language;
     this.textLanguage = activeTracks.text && activeTracks.text.language;
     this.advertising = player.config.plugins && player.config.plugins.ima;
+  }
+}
+
+/**
+ * Gets the time to start from with respect to live media.
+ * @param {KalturaPlayer} player - The player.
+ * @returns {number} - The time to start from (0 in live indicates the live edge).
+ */
+function getStartTime(player: KalturaPlayer): number {
+  if (player.isLive()) {
+    if (!player.isDvr()) {
+      return 0;
+    } else {
+      return player.duration - player.currentTime < player.config.cast.dvrThreshold ? 0 : player.currentTime;
+    }
+  } else {
+    return player.currentTime;
   }
 }
 
