28d098f29ed9e805c064d1a14adc145328caf57a
Dan Ziv <dan.ziv@kaltura.com>
2018-12-19 11:30:38 +0200

    fix(FEC-8773): local storage mute state is not updated when user drag volume bar (#190)
    Manipulate the muted value saved in the local storage in case user changed volume.

--- test/src/common/storage/storage-manager.spec.js
+++ test/src/common/storage/storage-manager.spec.js
@@ -1,16 +1,38 @@
 import StorageManager from '../../../../src/common/storage/storage-manager';
 import StorageWrapper from '../../../../src/common/storage/storage-wrapper';
+import * as TestUtils from '../../utils/test-utils';
+import {setup} from '../../../../src';
+import {FakeEvent} from '@playkit-js/playkit-js';
+
+const targetId = 'player-placeholder_storage-manager.spec';
 
 describe('StorageManager', function() {
-  let sandbox;
+  let config, player, sandbox;
+  const partnerId = 1091;
+  const entryId = '0_wifqaipd';
+
+  before(function() {
+    TestUtils.createElement('DIV', targetId);
+  });
 
   beforeEach(function() {
     sandbox = sinon.sandbox.create();
+    config = {
+      targetId: targetId,
+      provider: {
+        partnerId: partnerId
+      }
+    };
   });
 
   afterEach(function() {
     sandbox.restore();
+    TestUtils.removeVideoElementsFromTestPage();
     window.localStorage.clear();
+  });
+
+  after(function() {
+    TestUtils.removeElement(targetId);
   });
 
   it('should return it has no storage', function() {
@@ -67,6 +89,26 @@ describe('StorageManager', function() {
         textLanguage: 'heb',
         audioLanguage: 'eng'
       }
+    });
+  });
+
+  it('should set muted to true/false depends on changed volume', function() {
+    sandbox.stub(StorageWrapper, '_testForLocalStorage').callsFake(() => {
+      StorageWrapper._isLocalStorageAvailable = true;
+    });
+    player = setup(config);
+    player.loadMedia({entryId: entryId}).then(() => {
+      player.muted = true;
+      player.dispatchEvent(new FakeEvent(player.Event.UI.USER_CLICKED_MUTE));
+      StorageManager.getStorageConfig().playback.muted.should.be.true;
+      player.volume = 0.5;
+      player.dispatchEvent(new FakeEvent(player.Event.UI.USER_CHANGED_VOLUME));
+      StorageManager.getStorageConfig().playback.muted.should.be.false;
+      StorageManager.getStorageConfig().playback.volume.should.equals(0.5);
+      player.volume = 0;
+      player.dispatchEvent(new FakeEvent(player.Event.UI.USER_CHANGED_VOLUME));
+      StorageManager.getStorageConfig().playback.muted.should.be.true;
+      StorageManager.getStorageConfig().playback.volume.should.equals(0);
     });
   });
 });
