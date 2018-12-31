a69fb9873d5b959b543ae6818be97d50d88df4ec
yair.ansbacher <yairans@gmail.com>
2018-12-31 15:03:08 +0200

    fix: set playlist by configure method doesn't work

--- src/kaltura-player.js
+++ src/kaltura-player.js
@@ -129,6 +129,9 @@ class KalturaPlayer extends FakeEventTarget {
     if (config.ui) {
       this._uiWrapper.setConfig(config.ui);
     }
+    if (config.playlist) {
+      this._playlistManager.configure(config.playlist);
+    }
   }
 
   ready(): Promise<*> {
