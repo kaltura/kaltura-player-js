fe058869d162dd81476b0775275db3164d41d7dd
yair.ansbacher <yairans@gmail.com>
2019-07-07 12:55:06 +0300

    feat(FEC-9156): getting bumper from playback context

--- src/kaltura-player.js
+++ src/kaltura-player.js
@@ -79,7 +79,7 @@ class KalturaPlayer extends FakeEventTarget {
     Utils.Object.mergeDeep(playerConfig.sources, this._localPlayer.config.sources);
     Utils.Object.mergeDeep(playerConfig.session, this._localPlayer.config.session);
     Object.keys(this._localPlayer.config.plugins).forEach(name => {
-      playerConfig.plugins[name] = {};
+      playerConfig.plugins[name] = playerConfig.plugins[name] || {};
     });
     addKalturaPoster(playerConfig.sources, mediaConfig.sources, this._localPlayer.dimensions);
     addKalturaParams(this, playerConfig);
