a3a5adbe1468a6d79516d2bd8f692540dbb9a27b
OrenMe <oren.me@gmail.com>
2019-08-07 10:35:25 +0300

    update provider deps and type

--- flow-typed/modules/playkit-js-providers.js
+++ flow-typed/modules/playkit-js-providers.js
@@ -1,8 +1,4 @@
 // @flow
-declare module 'playkit-js-providers' {
-  declare module.exports: any;
-}
-
 declare module '@playkit-js/core-provider/src/base-provider' {
   declare module.exports: any;
 }
