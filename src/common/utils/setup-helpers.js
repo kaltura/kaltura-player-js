4280dc5ce8c694b215615b5688cb991d6b84dc54
Yuvalke <48407737+Yuvalke@users.noreply.github.com>
2019-11-07 18:52:28 +0200

    fix: config keySystem isn't boolean (#283)
    check for existence string instead boolean

--- src/common/utils/setup-helpers.js
+++ src/common/utils/setup-helpers.js
@@ -404,7 +404,7 @@ function configureLGTVDefaultOptions(options: KPOptionsObject): void {
 function configureEdgeDRMDefaultOptions(options: KPOptionsObject): void {
   if (Env.browser.name === 'Edge') {
     const keySystem = Utils.Object.getPropertyPath(options, 'drm.keySystem');
-    if (typeof keySystem !== 'boolean') {
+    if (!keySystem) {
       options = Utils.Object.createPropertyPath(options, 'drm.keySystem', DrmScheme.PLAYREADY);
     }
   }
