6da651889215c04905341dc1860ad9c14e7d97fc
Yuval Keidar <48407737+Yuvalke@users.noreply.github.com>
2020-10-06 12:06:17 +0300

    fix: incorrect order in import dependencies break IE11 (#362)
    Issue: getLogger comes from playkit which loads all the imported files in playkit.js, in that moment polyfill doesn't exist yet which causes crash on IE11.
    Solution: remove logger from polyfill.
    refactor which relocates logger to Kaltura player will return those logs back.

--- src/common/polyfills/polyfill-manager.js
+++ src/common/polyfills/polyfill-manager.js
@@ -1,21 +1,16 @@
 // @flow
-import {getLogger} from '@playkit-js/playkit-js';
-
 if (!window._babelPolyfill) {
   require('@babel/polyfill');
 }
 
 export default class PolyfillManager {
   static _polyfills: Array<Function> = [];
-  static _logger: any = getLogger('PolyfillManager');
 
   static register(polyfill: Function): void {
-    PolyfillManager._logger.debug('Register <' + polyfill.id + '> polyfill');
     PolyfillManager._polyfills.push(polyfill);
   }
 
   static installAll(): void {
-    PolyfillManager._logger.debug('Installing all polyfills');
     for (let i = 0; i < PolyfillManager._polyfills.length; i++) {
       PolyfillManager._polyfills[i].install();
     }
--- src/common/polyfills/prepend.js
+++ src/common/polyfills/prepend.js
@@ -1,18 +1,14 @@
 // @flow
 import PolyfillManager from './polyfill-manager';
-import {getLogger} from '@playkit-js/playkit-js';
 
 export default class PrependPolyfill {
   static id: string = 'prepend';
-  static _logger: any = getLogger('PrependPolyfill');
 
   static install(): void {
     [Element.prototype, Document.prototype, DocumentFragment.prototype].forEach(function (item) {
       if (Object.prototype.hasOwnProperty.call(item, 'prepend')) {
-        PrependPolyfill._logger.debug('No need to install polyfill on item', item);
         return;
       }
-      PrependPolyfill._logger.debug('Installing polyfill on item', item);
       // $FlowFixMe
       Object.defineProperty(item, 'prepend', {
         configurable: true,
