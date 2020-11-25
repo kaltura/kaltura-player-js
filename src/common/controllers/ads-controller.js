e350cbeefc37d03187650c010f8c3b9c8c0ac395
Yuval Keidar <48407737+Yuvalke@users.noreply.github.com>
2020-11-25 12:25:35 +0200

    fix(FEC-10694): Ima post roll overrides the post bumper (#378)
    Issue: the post bumper overridden by the post roll.
    Solution: should wait to onPromiseEnded of bumper resolved and then call ima onPromiseEnded.

--- src/common/controllers/ads-controller.js
+++ src/common/controllers/ads-controller.js
@@ -324,18 +324,17 @@ class AdsController extends FakeEventTarget implements IAdsController {
     }
     const bumperCtrl = this._adsPluginControllers.find(controller => this._isBumper(controller));
     const adCtrl = this._adsPluginControllers.find(controller => !this._isBumper(controller));
-    const bumperCompletePromise = bumperCtrl && typeof bumperCtrl.onPlaybackEnded === 'function' ? bumperCtrl.onPlaybackEnded() : Promise.resolve();
-    const adCompletePromise = adCtrl && typeof adCtrl.onPlaybackEnded === 'function' ? adCtrl.onPlaybackEnded() : Promise.resolve();
+    const bumperCompleted =
+      bumperCtrl && typeof bumperCtrl.onPlaybackEnded === 'function' ? () => bumperCtrl.onPlaybackEnded() : () => Promise.resolve();
+    const adCompleted = adCtrl && typeof adCtrl.onPlaybackEnded === 'function' ? () => adCtrl.onPlaybackEnded() : () => Promise.resolve();
     if (!(this._adBreaksLayout.includes(-1) || this._adBreaksLayout.includes('100%'))) {
       this._allAdsCompleted = true;
-    } else {
-      const playbackEndedHandler = () => this._handleConfiguredPostroll();
-      // $FlowFixMe
-      bumperCompletePromise.finally(() => {
-        // $FlowFixMe
-        adCompletePromise.finally(playbackEndedHandler);
-      });
     }
+    // $FlowFixMe
+    bumperCompleted().finally(() => {
+      // $FlowFixMe
+      adCompleted().finally(() => this._handleConfiguredPostroll());
+    });
   }
 
   _onPlaybackEnded(): void {
