425b2e4bc712d095789e3dd8061895db47fcbbfd
Roy Bregman <48884909+RoyBregman@users.noreply.github.com>
2020-11-30 13:23:23 +0200

    chore(FEC-10233): fixed async plugin loading unit tests (#382)
    changes unit tests of async plugin loading:
    
    wrapped with try catch
    removed timed check and replaced with promise resolved / rejected
    changed dummy promises to create the promise once and not upon every getter call
    destroyed plugins and player after each test

--- test/src/common/plugin/test-plugins/async-reject-plugin.js
+++ test/src/common/plugin/test-plugins/async-reject-plugin.js
@@ -1,4 +1,5 @@
 import {BasePlugin} from '../../../../../src/common/plugins';
+import {Utils} from '@playkit-js/playkit-js';
 
 export default class AsyncRejectPlugin extends BasePlugin {
   static DELAY_ASYNC = 300;
@@ -6,11 +7,16 @@ export default class AsyncRejectPlugin extends BasePlugin {
     return true;
   }
 
+  promise: DeferredPromise;
+
+  constructor(name: string, player: Object, config: Object) {
+    super(name, player, config);
+    this.promise = Utils.Object.defer();
+    setTimeout(() => {
+      this.promise.reject();
+    }, AsyncRejectPlugin.DELAY_ASYNC);
+  }
   get ready(): Promise<*> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject();
-      }, AsyncRejectPlugin.DELAY_ASYNC);
-    });
+    return this.promise;
   }
 }
