9df005634a500e3ceb73bb9ca495a1408fedc8d2
yair.ansbacher <yairans@gmail.com>
2021-02-18 16:32:57 +0200

    add tests

---
+++ test/src/ott/player-defaults.spec.js
@@ -0,0 +1,28 @@
+import {getDefaultRedirectOptions} from './../../../src/ott/player-defaults';
+
+describe('getDefaultRedirectOptions', function () {
+  let sandbox;
+  beforeEach(function () {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+  describe('redirectExternalStreamsHandler', function () {
+    it('should return the default', function () {
+      const defaultConfig = getDefaultRedirectOptions({});
+      (typeof defaultConfig.sources.options.redirectExternalStreamsHandler === 'function').should.be.true;
+    });
+
+    it('should return void if already configured on player config', function () {
+      const defaultConfig = getDefaultRedirectOptions({sources: {options: {redirectExternalStreamsHandler: () => {}}}});
+      (defaultConfig.sources === undefined).should.be.true;
+    });
+
+    it('should return void if already configured on media config', function () {
+      const defaultConfig = getDefaultRedirectOptions({}, {sources: {options: {redirectExternalStreamsHandler: () => {}}}});
+      (defaultConfig.sources === undefined).should.be.true;
+    });
+  });
+});
