b6ed0d1e5e3732416080c8dfb6362842b8c83a60
omriKaltrua <68190592+omriKaltura@users.noreply.github.com>
2021-05-06 19:06:56 +0300

    Update after_deploy.sh
    adding webhook to invoke ci on new jenkins

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 curl -k -d "{'version':$1}" -H "Content-Type: application/json" -X POST https://jenkins.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$2
+curl -k -d "{'version':$1}" -H "Content-Type: application/json" -X POST https://jenkins-central.prod.ovp.kaltura.com/generic-webhook-trigger/invoke?token=$3
