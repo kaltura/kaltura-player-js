8937aea9774c2eb988e80521dfa531907a7f328e
Yuval Keidar <48407737+Yuvalke@users.noreply.github.com>
2021-06-06 12:46:39 +0300

    build(FEC-10700): Improvement for CI/CD (#389)
    1. add the number of commits from the master.
    2. releases from the no-master branch won’t be taken to beta/latest release.
    3. curl request check failure.
    4. don't stop the release after conventional-github-releaser failed. 
    5. update dependencies before tests so they'll be included

---
+++ scripts/travis.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
+set -ev
+yarn install
+
+if [[ "$TRAVIS_BRANCH" = "master" ]] && [[ "$TRAVIS_EVENT_TYPE" != "pull_request" ]] && [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ ^(chore).*(update dist)$ ]] && [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ ^chore\(release\) ]]; then
+  echo "Prepare Canary"
+  yarn upgrade @playkit-js/playkit-js@canary
+  yarn upgrade @playkit-js/playkit-js-dash@canary
+  yarn upgrade @playkit-js/playkit-js-hls@canary
+  yarn upgrade @playkit-js/playkit-js-ui@canary
+  yarn upgrade playkit-js-providers@https://github.com/kaltura/playkit-js-providers.git#master
+fi
+
+if [ "${TRAVIS_MODE}" = "lint" ]; then
+  yarn run eslint
+elif [ "${TRAVIS_MODE}" = "flow" ]; then
+  yarn run flow
+elif [ "${TRAVIS_MODE}" = "unitTests" ]; then
+	yarn run test
+elif [ "${TRAVIS_MODE}" = "release" ] || [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+  if [ "${TRAVIS_MODE}" = "releaseCanary" ]; then
+    echo "Run standard-version"
+    yarn run release --prerelease canary --skip.commit=true --skip.tag=true
+    sha=$(git rev-parse --verify --short HEAD)
+    echo "Current sha ${sha}"
+    commitNumberAfterTag=$(git rev-list  `git rev-list --tags --no-walk --max-count=1`..HEAD --count)
+    echo "Number of commit from last tag ${commitNumberAfterTag}"
+    currentVersion=$(npx -c 'echo "$npm_package_version"')
+    echo "Current version ${currentVersion}"
+    newVersion=$(echo $currentVersion | sed -e "s/canary\.[[:digit:]]/canary.${commitNumberAfterTag}-${sha}/g")
+    echo "New version ${newVersion}"
+    sed -iE "s/$currentVersion/$newVersion/g" package.json
+    sed -iE "s/$currentVersion/$newVersion/g" CHANGELOG.md
+    rm package.jsonE
+    rm CHANGELOG.mdE
+    echo "Building..."
+    yarn run build:ovp && yarn run build:ott && npm run commit:dist
+    echo "Finish building"
+    git push https://$GH_TOKEN@github.com/kaltura/kaltura-player-js "master" > /dev/null 2>&1
+    echo "Push Build to origin"
+  else
+    echo "Run conventional-github-releaser"
+    #ignore error to make sure release won't get stuck
+    conventional-github-releaser -p angular -t $GH_TOKEN || true
+  fi
+else
+	echo "Unknown travis mode: ${TRAVIS_MODE}" 1>&2
+	exit 1
+fi
