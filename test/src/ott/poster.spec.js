58266579d020e09e37a1029a2c2fbbab39a3b18e
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2021-11-02 13:19:07 +0200

    addKalturaPoster tests

---
+++ test/src/ott/poster.spec.js
@@ -0,0 +1,27 @@
+import {addKalturaPoster} from '../../../src/ott/poster';
+
+describe('addKalturaPoster', function () {
+  it('should change poster of mediaSources to string', function () {
+    const mediaSources = {
+      poster: [
+        {url: 'https//my/kaltura/poster', width: 0, height: 0},
+        {url: 'https//my/kaltura/poster', width: 0, height: 0}
+      ]
+    };
+    const playerSources = {poster: 'https//my/kaltura/poster'};
+    addKalturaPoster(playerSources, mediaSources, {width: 640, height: 360});
+    mediaSources.poster.should.equal(playerSources.poster);
+  });
+
+  it('should align poster of mediaSources with poster of playerSources', function () {
+    const mediaSources = {poster: 'https//my/kaltura/poster'};
+    const playerSources = {
+      poster: [
+        {url: 'https//my/kaltura/poster', width: 0, height: 0},
+        {url: 'https//my/kaltura/poster', width: 0, height: 0}
+      ]
+    };
+    addKalturaPoster(playerSources, mediaSources, {width: 640, height: 360});
+    mediaSources.poster.should.equal(playerSources.poster);
+  });
+});
