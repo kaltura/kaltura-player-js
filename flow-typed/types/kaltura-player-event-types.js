f4c1f9d40c91aa27c46fe7054fa5a6bb0e69aedb
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-01-12 14:03:06 +0200

    remove unused event

--- flow-typed/types/kaltura-player-event-types.js
+++ flow-typed/types/kaltura-player-event-types.js
@@ -3,6 +3,5 @@ declare type KPEventTypes = {
   Core: PKEventTypes,
   UI: {[event: string]: string},
   Cast: {[event: string]: string},
-  Playlist: {[event: string]: string},
-  Related: {[event: string]: string}
+  Playlist: {[event: string]: string}
 };
--- src/common/related/related-event-type.js
+++
@@ -1,8 +0,0 @@
-// @flow
-const namespace = 'kaltura-player';
-
-const RelatedEventType: {[event: string]: string} = {
-  RELATED_ITEM_CHANGED: `${namespace}-relateditemchanged`
-};
-
-export {RelatedEventType};
