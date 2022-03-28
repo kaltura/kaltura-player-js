e29204ac285a791e0c8ff11a5a15793f398c0b5e
Sivan Agranov <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-03-28 19:58:49 +0300

    fix: add fake-event-target to ts types (#534)
    Used by related plugin.

--- ts-typed/fake-event-target.d.ts
+++ ts-typed/fake-event-target.d.ts
@@ -1,13 +1,9 @@
-declare function EventListener(event: KalturaPlayerTypes.FakeEvent): boolean | void;
+interface FakeEvent {}
 
 declare namespace KalturaPlayerTypes {
   export interface FakeEventTarget {
-    addEventListener(type: string, listener: EventListener);
-
-    removeEventListener(type: string, listener: EventListener);
-
-    registerPlugin(name: string, component: any): void;
-
-    dispatchEvent(event: KalturaPlayerTypes.FakeEvent);
+    addEventListener(type: string, listener: (...args: any) => void);
+    removeEventListener(type: string, listener: (...args: any) => void);
+    dispatchEvent(event: FakeEvent);
   }
 }
