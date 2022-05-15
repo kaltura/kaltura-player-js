27f070ddda7ab66d7f0bda274def582791ee61b7
JonathanTGold <jonathan.gold@86@gmail.com>
2022-05-15 18:45:18 +0300

    fix types

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,9 @@
 import {Store} from 'redux';
 import * as preactLib from 'preact';
-import {BasePlugin} from "kaltura-player-js";
+import {BasePlugin} from 'kaltura-player-js';
 
 declare module 'kaltura-player-js' {
-  export const ui: typeof PlaykitUI
+  export const ui: typeof PlaykitUI;
   export class BasePlugin {
     protected static defaultConfig: {};
     protected constructor(name: string, player: KalturaPlayer);
@@ -14,17 +14,17 @@ declare module 'kaltura-player-js' {
     public destroy(): void;
     public static isValid(): boolean;
   }
-  export function registerPlugin(pluginName: string, plugin: IBasePlugin): void
+  export function registerPlugin(pluginName: string, plugin: IBasePlugin): void;
 }
 
 export interface IBasePlugin {
-  new(name: string, player: KalturaPlayer): BasePlugin;
+  new (name: string, player: KalturaPlayer): BasePlugin;
 }
 
 export class KalturaPlayer {
   public registerService(pluginName: string, service: Object): void;
   public getService<T>(serviceName: string): T;
-  public get ui(): UIWrapper
+  public get ui(): UIWrapper;
 }
 
 export class UIWrapper {
@@ -49,59 +49,63 @@ declare module PlaykitUI {
   export type SidePanelPosition = 'top' | 'bottom' | 'right' | 'left';
   export type SidePanelMode = 'alongside' | 'hidden' | 'over';
   export type ReservedPresetName = 'Playback' | 'Live' | 'Idle' | 'Ads' | 'Error';
-  export  const SidePanelOrientation: {
-    VERTICAL: 'vertical',
-    HORIZONTAL: 'horizontal'
+  export const SidePanelOrientation: {
+    VERTICAL: 'vertical';
+    HORIZONTAL: 'horizontal';
   };
 
   export const SidePanelPositions: {
-    LEFT: 'left',
-    TOP: 'top',
-    BOTTOM: 'bottom',
-    RIGHT: 'right'
+    LEFT: 'left';
+    TOP: 'top';
+    BOTTOM: 'bottom';
+    RIGHT: 'right';
   };
 
   export const SidePanelModes: {
-    ALONGSIDE: 'alongside',
-    HIDDEN: 'hidden',
-    OVER: 'over'
+    ALONGSIDE: 'alongside';
+    HIDDEN: 'hidden';
+    OVER: 'over';
   };
 
   export const ReservedPresetNames: {
-    Playback: 'Playback',
-    Live: 'Live'
-    Idle: 'Idle'
-    Ads: 'Ads'
-    Error: 'Error'
+    Playback: 'Playback';
+    Live: 'Live';
+    Idle: 'Idle';
+    Ads: 'Ads';
+    Error: 'Error';
   };
 
   export const ReservedPresetAreas: {
-    PresetFloating: 'PresetFloating',
-    BottomBarLeftControls: 'BottomBarLeftControls',
-    BottomBarRightControls: 'BottomBarRightControls',
-    TopBarLeftControls: 'TopBarLeftControls',
-    TopBarRightControls: 'TopBarRightControls',
-    SidePanelTop: 'SidePanelTop',
-    SidePanelLeft: 'SidePanelLeft',
-    SidePanelRight: 'SidePanelRight',
-    SidePanelBottom: 'SidePanelBottom',
-    PresetArea: 'PresetArea',
-    InteractiveArea: 'InteractiveArea',
-    PlayerArea: 'PlayerArea',
-    VideoArea: 'VideoArea'
+    PresetFloating: 'PresetFloating';
+    BottomBarLeftControls: 'BottomBarLeftControls';
+    BottomBarRightControls: 'BottomBarRightControls';
+    TopBarLeftControls: 'TopBarLeftControls';
+    TopBarRightControls: 'TopBarRightControls';
+    SidePanelTop: 'SidePanelTop';
+    SidePanelLeft: 'SidePanelLeft';
+    SidePanelRight: 'SidePanelRight';
+    SidePanelBottom: 'SidePanelBottom';
+    PresetArea: 'PresetArea';
+    InteractiveArea: 'InteractiveArea';
+    PlayerArea: 'PlayerArea';
+    VideoArea: 'VideoArea';
   };
 
   export const reducers: {
     shell: {
       actions: {
-        updateSidePanelMode: (position: SidePanelPosition, sidePanelMode: SidePanelMode) => ({
-          type: string,
+        updateSidePanelMode: (
           position: SidePanelPosition,
           sidePanelMode: SidePanelMode
-        }),
-      }
-    }
+        ) => {
+          type: string;
+          position: SidePanelPosition;
+          sidePanelMode: SidePanelMode;
+        };
+      };
+    };
   };
+  export const style: {[cssClassName: string]: string};
   export const h: typeof preact.h;
-  export const preact: typeof preactLib
+  export const preact: typeof preactLib;
 }
