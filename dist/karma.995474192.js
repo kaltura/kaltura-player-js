d93d07a90b36e3b2f333a037726599d139f2a42e
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2022-06-29 16:01:50 +0300

    chore: update dist

---
+++ dist/karma.995474192.js
@@ -0,0 +1,3 @@
+var KalturaPlayer =
+(window["webpackJsonpKalturaPlayer"] = window["webpackJsonpKalturaPlayer"] || []).push([["karma.995474192"],[],[["../test/setup/karma.js","runtime","commons"]]]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJrYXJtYS45OTU0NzQxOTIuanMiLCJzb3VyY2VSb290IjoiIn0=
