2fb7bd40a269df6286a74eea01fb61ffa33298a5
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2022-09-25 13:19:10 +0200

    fix(FEC-12097): More plugins - Update relevant types (#563)
    Update relevant TS types
    
    solves: FEC-12097
    
    related pr:
    kaltura/playkit-js-ui-managers#10

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,11 @@
 import {Store} from 'redux';
 import * as preactLib from 'preact';
+import * as preacti18nLib from 'preact-i18n';
+import * as reduxLib from 'react-redux';
+import {BasePlugin} from 'kaltura-player-js';
 
 declare module 'kaltura-player-js' {
+  export function setup(config: any): KalturaPlayer;
   export const ui: typeof PlaykitUI;
   export class FakeEvent implements CustomEvent {
     readonly payload: any;
@@ -34,27 +38,44 @@ declare module 'kaltura-player-js' {
   export class BasePlugin<ConfigType> {
     protected config: ConfigType;
     protected static defaultConfig: {};
-    protected constructor(name: string, player: KalturaPlayer, config: ConfigType);
+    protected constructor(name: string, player: KalturaPlayer, config?: ConfigType);
     protected logger: Logger;
     protected loadMedia(): void;
     public player: KalturaPlayer;
     public reset(): void;
     public destroy(): void;
     public static isValid(): boolean;
-    public eventManager: any;
+    public eventManager: PlaykitUI.EventManager;
   }
   export function registerPlugin<ConfigType>(pluginName: string, plugin: IBasePlugin<ConfigType>): void;
 }
 
+type Target = KalturaPlayer | HTMLElement | Document;
+type CallbackFunction = (...args: any) => void;
+
 export interface IBasePlugin<ConfigType> {
-  new (name: string, player: KalturaPlayer, config: ConfigType): BasePlugin<ConfigType>;
+  new (name: string, player: KalturaPlayer, config?: ConfigType): BasePlugin<ConfigType>;
 }
 
 export class KalturaPlayer {
+  public setMedia(config: any): void;
   public registerService(pluginName: string, service: Object): void;
   public getService<T>(serviceName: string): T;
   public get ui(): UIWrapper;
   public get Event(): EventTypes;
+  public get paused(): boolean;
+  public configure(config: Object): void;
+  public ready(): Promise<void>;
+  public load(): void;
+  public play(): void;
+  public pause(): void;
+  public getView(): HTMLElement;
+  public getVideoElement(): HTMLVideoElement;
+  public reset(isChangeMedia?: boolean): void;
+  public isLive(): boolean;
+  public isOnLiveEdge(): boolean;
+  public isDvr(): boolean;
+  public seekToLiveEdge(): void;
 }
 
 export class UIWrapper {
@@ -83,6 +104,14 @@ export interface EventTypes {
 }
 
 declare module PlaykitUI {
+  export class EventManager {
+    listen: (target: Target, event: string, cb: CallbackFunction) => void;
+    listenOnce: (target: Target, event: string, cb: CallbackFunction) => void;
+    unlisten: (target: Target, event: string, cb?: CallbackFunction) => void;
+    destroy: () => void;
+    removeAll: () => void;
+  }
+
   export type SidePanelPosition = 'top' | 'bottom' | 'right' | 'left';
   export type SidePanelMode = 'alongside' | 'hidden' | 'over';
   export type ReservedPresetName = 'Playback' | 'Live' | 'Idle' | 'Ads' | 'Error';
@@ -142,7 +171,37 @@ declare module PlaykitUI {
       };
     };
   };
+
+  enum IconState {
+    INACTIVE = 0,
+    ACTIVE = 1
+  }
+
+  export const Components: {
+    Tooltip: preactLib.ComponentClass<{label: string}>;
+    Icon: preactLib.ComponentClass<{
+      id: string | number;
+      path: string;
+      state?: IconState;
+      color?: string;
+      activeColor?: string;
+      width?: number;
+      height?: number;
+      viewBox?: string;
+    }>;
+    PLAYER_SIZE: {
+      TINY: 'tiny';
+      EXTRA_SMALL: 'extrasmall';
+      SMALL: 'small';
+      MEDIUM: 'medium';
+      LARGE: 'large';
+      EXTRA_LARGE: 'extralarge';
+    };
+  };
   export const style: {[cssClassName: string]: string};
   export const h: typeof preact.h;
   export const preact: typeof preactLib;
+  export const redux: typeof reduxLib;
+  export const preacti18n: typeof preacti18nLib;
+  export const Event: any;
 }
