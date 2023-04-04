99f83910cbce720b55da1f3a153a47b88e46a678
Serhii Marchenko <serhii.marchenko@kaltura.com>
2023-04-04 11:27:00 +0300

    Revert "fix(FEC-13076): Safari track.cues issue (#612)"
    This reverts commit 963a8d27ded4f4470b992a6f9d4e57ca29229536.

--- src/common/cuepoint/cuepoint-manager.js
+++ src/common/cuepoint/cuepoint-manager.js
@@ -45,9 +45,7 @@ export class CuePointManager {
     let cue = null;
     const metadataTracks = this._getMetadataTracks();
     metadataTracks.some(track => {
-      if (track.cues) {
-        cue = track.cues.getCueById(id);
-      }
+      cue = track.cues.getCueById(id);
       return cue;
     });
     return cue;
