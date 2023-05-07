985d55a99bf2cee8c27263dd2f0810f2601e9d33
Gilad Nadav <gilad.nadav@kaltura.com>
2023-05-07 15:56:02 +0300

    chore(FEC-13123): replace travis with github actions

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,7 @@
-const isWindows = /^win/.test(process.platform);
-const isMacOS = /^darwin/.test(process.platform);
 // Create custom launcher in case running with Travis
-const customLaunchers = {
-  Chrome_travis_ci: {
-    base: 'Chrome',
-    flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
-  }
-};
-
 const launchers = {
   Chrome_browser: {
-    base: 'Chrome',
+    base: 'ChromeHeadless',
     flags: ['--no-sandbox', '--autoplay-policy=no-user-gesture-required']
   }
 };
@@ -19,7 +10,7 @@ module.exports = config => {
   const karmaConf = {
     logLevel: config.LOG_INFO,
     customLaunchers: launchers,
-    browsers: ['Chrome_browser', 'Firefox'],
+    browsers: [],
     concurrency: 1,
     singleRun: true,
     colors: true,
@@ -58,16 +49,7 @@ module.exports = config => {
     }
   };
 
-  if (process.env.TRAVIS) {
-    karmaConf.customLaunchers = customLaunchers;
-    karmaConf.browsers = ['Chrome_travis_ci'];
-  } else {
-    if (isWindows) {
-      karmaConf.browsers.push('IE');
-    } else if (isMacOS) {
-      karmaConf.browsers.push('Safari');
-    }
-  }
-
+  karmaConf.customLaunchers = launchers;
+  karmaConf.browsers = ['Chrome_browser'];
   config.set(karmaConf);
 };
