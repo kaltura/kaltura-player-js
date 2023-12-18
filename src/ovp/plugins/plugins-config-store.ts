d79cd207db95bcd62a9eee4843db7fca59c8832b
JonathanTGold <jonathan.gold@86@gmail.com>
2023-12-18 13:19:17 +0200

    ffix

--- src/ovp/plugins/plugins-config-store.ts
+++ src/ovp/plugins/plugins-config-store.ts
@@ -1,13 +1,11 @@
-//@flow
+import { BasePluginConfigStore, templateRegex } from '../../common/plugins/plugins-config-store';
 
-import {BasePluginConfigStore, templateRegex} from '../../common/plugins/plugins-config-store';
-
-import {Utils} from '@playkit-js/playkit-js';
+import { Utils } from '@playkit-js/playkit-js';
 
 class PluginConfigStore extends BasePluginConfigStore {
-  _ovpPluginsConfig: {[pluginName: string]: Object} = {
-    ottAnalytics: {}
-  };
+    private _ovpPluginsConfig: { [pluginName: string]: any } = {
+        ottAnalytics: {}
+    };
 
   constructor() {
     super();
