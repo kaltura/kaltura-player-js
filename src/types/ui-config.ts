0d3da4d39eb81ab4d1b57d14ddce78555ddd040c
JonathanTGold <jonathan.gold@86@gmail.com>
2024-01-02 17:48:59 +0200

    fix type

--- src/types/ui-config.ts
+++ src/types/ui-config.ts
@@ -5,4 +5,5 @@ export interface UiConfig extends UIOptionsObject {
   disable?: boolean;
   css?: string;
   customPreset?: { template: () => any; condition: () => any }[];
+  targetId: string // TODO temp!! delete after fix
 }
