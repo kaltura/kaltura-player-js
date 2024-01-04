76ac2ac61576139e008211f044b8ebcd95434de9
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-01-04 11:39:11 +0200

    fix doc

--- src/types/playlist/playlist-options.ts
+++ src/types/playlist/playlist-options.ts
@@ -2,7 +2,7 @@
  * @typedef {Object} PlaylistOptions
  * @property {boolean} [autoContinue=true] - Determines whether to continue to the next item automatically.
  * @property {boolean} [loop=false] - Determines whether to play the playlist in a loop. When selected, the playlist will play automatically even if autoContinue is set to false.
- * @property {boolean} [startAtEntryId] - Determines which entry id to start to the play the playlist from.
+ * @property {string} [startAtEntryId] - Determines which entry id to start to the play the playlist from.
  */
 export interface PlaylistOptions {
   autoContinue: boolean;
