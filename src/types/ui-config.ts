9fbc013b5a736fb181aa984a80891047b087bd83
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2024-01-09 14:04:54 +0200

    fix(FEC-13614): Fix types and webpack settings (#690)
    Resolves FEC-13614
    Related prs:
    kaltura/playkit-js-ui#838
    
    ---------
    
    Co-authored-by: JonathanTGold <jonathan.gold@86@gmail.com>

--- src/types/ui-config.ts
+++ src/types/ui-config.ts
@@ -1,9 +1,7 @@
-// TODO extends UIOptionsObject {
 import { UIOptionsObject } from '@playkit-js/playkit-js-ui';
 
 export interface UiConfig extends UIOptionsObject {
   disable?: boolean;
   css?: string;
   customPreset?: { template: () => any; condition: () => any }[];
-  targetId: string // TODO temp!! delete after fix
 }
