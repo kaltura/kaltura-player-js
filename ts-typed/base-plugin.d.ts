7a42a47763a89f1543948a54fe84f18549103180
JonathanTGold <jonathan.gold@86@gmail.com>
2024-01-10 13:33:26 +0200

    fix(/FEC-13635): Handle backward compatibility for customers relaying on old kaltura-player TS stystem

---
+++ ts-typed/base-plugin.d.ts
@@ -0,0 +1,14 @@
+declare namespace KalturaPlayerTypes {
+  export interface BasePlugin {
+    player: KalturaPlayerTypes.Player;
+    eventManager: KalturaPlayerTypes.EventManager;
+    config: any;
+    logger: KalturaPlayerTypes.Logger;
+    getName(): string;
+    destroy(): void;
+    dispatchEvent(name: string, payload?: any): void;
+    loadMedia(): void;
+    updateConfig(update: Object): void;
+    ready: Promise<void>;
+  }
+}
