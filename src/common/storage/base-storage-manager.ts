111e14f68a83cd2c6d877ef8d260f137d335cc16
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2024-02-06 14:05:33 +0200

    use this

--- src/common/storage/base-storage-manager.ts
+++ src/common/storage/base-storage-manager.ts
@@ -152,7 +152,7 @@ export class BaseStorageManager {
     const obj = {};
     Object.keys(this.StorageKeys).forEach((key) => {
       const value = this.StorageKeys[key];
-      const item = BaseStorageManager.getItem(value);
+      const item = this.getItem(value);
       // dont change to !==, it effects the logic
       // eslint-disable-next-line
       if (item != null) {
