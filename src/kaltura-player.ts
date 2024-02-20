6f12c5d8a340ea7951f4e66dd0f663fcd5d53bdc
Moshe Maor <moshe.mar@kaltura.com>
2024-02-20 15:53:33 +0200

    fix(SUP-41054): fix types comparison

--- src/kaltura-player.ts
+++ src/kaltura-player.ts
@@ -182,7 +182,7 @@ export class KalturaPlayer extends FakeEventTarget {
     const playerConfig = Utils.Object.copyDeep(mediaConfig);
     //merge the current sources from player to keep the sources passed from constructor earlier
     const sources = Utils.Object.mergeDeep({}, playerConfig.sources, this._localPlayer.sources);
-    if (playerConfig.sources.type === MediaType.VOD && this._localPlayer.sources.mediaEntryType === MediaType.LIVE) {
+    if (playerConfig.sources.type === MediaType.VOD && this._localPlayer.sources.mediaEntryType?.toString() === MediaType.LIVE) {
       sources.id = playerConfig.sources.id;
     }
     delete playerConfig.sources;
