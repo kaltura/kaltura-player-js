0be7f404c9c5c66849cd3cbb45f27ca5a4d0945f
Moshe Maor <moshe.maor@kaltura.com>
2024-07-17 16:54:01 +0300

    Update kaltura-params.ts

--- src/common/utils/kaltura-params.ts
+++ src/common/utils/kaltura-params.ts
@@ -127,7 +127,7 @@ function getReferrer(): string {
 
 function isInUnfriendlyIframe(): boolean {
   try {
-    return (window.parent.document.URL !== window.parent.document.URL);;
+    return window.parent.document.URL !== window.parent.document.URL;
   } catch {
     return true;
   }
