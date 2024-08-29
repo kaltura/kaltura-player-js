fb06882c645a4989b0b5b52f1fb402c44e099398
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2024-08-29 12:09:27 +0300

    add check if window.kalturaCustomPresetMap already on the window

--- src/common/ui-wrapper.ts
+++ src/common/ui-wrapper.ts
@@ -26,7 +26,7 @@ class UIWrapper {
       }
     } else {
       this._uiManager = new UIManager(player, config);
-      if (config.customPreset || window.kalturaCustomPresetMap[config.targetId]) {
+      if (config.customPreset || (window.kalturaCustomPresetMap && window.kalturaCustomPresetMap[config.targetId])) {
         this._uiManager.buildCustomUI(config.customPreset || window.kalturaCustomPresetMap[config.targetId]);
       } else {
         this._uiManager.buildDefaultUI();
