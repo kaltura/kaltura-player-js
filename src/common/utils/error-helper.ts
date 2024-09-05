5114c85f2ba6556eeb7a9574c10c948dae4d3ff9
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2024-09-05 11:00:40 +0300

    fix(SUP-43014): [elearning] specified error message for v7 for IP restriction

--- src/common/utils/error-helper.ts
+++ src/common/utils/error-helper.ts
@@ -5,15 +5,18 @@ const isBlockAction = (error: Error): boolean => error.code === 2001;
 const isMediaNotReady = (error: Error): boolean => error.code === 2002;
 const isGeolocationRestricted = (error: Error): boolean => error.data?.messages && error.data?.messages[0].code === 'COUNTRY_RESTRICTED';
 const isSessionRestricted = (error: Error): boolean => error.data?.messages && error.data?.messages[0].code === 'SESSION_RESTRICTED';
+const isIPRestricted = (error: Error): boolean => error.data?.messages && error.data?.messages[0].code === 'IP_RESTRICTED';
 
 const isSessionRestrictedError = (error: Error): boolean => isBackEndError(error) && isBlockAction(error) && isSessionRestricted(error);
 const isGeolocationError = (error: Error): boolean => isBackEndError(error) && isBlockAction(error) && isGeolocationRestricted(error);
 const isMediaNotReadyError = (error: Error): boolean => isBackEndError(error) && isMediaNotReady(error);
+const isIPRestrictedError = (error: Error): boolean => isBackEndError(error) && isBlockAction(error) && isIPRestricted(error);
 
 const conditionsToErrors: any[] = [
   [isSessionRestrictedError, Error.Category.MEDIA_UNAVAILABLE],
   [isGeolocationError, Error.Category.GEO_LOCATION],
-  [isMediaNotReadyError, Error.Category.MEDIA_NOT_READY]
+  [isMediaNotReadyError, Error.Category.MEDIA_NOT_READY],
+  [isIPRestrictedError, Error.Category.IP_RESTRICTED]
 ];
 
 function getErrorCategory(error: Error): number {
