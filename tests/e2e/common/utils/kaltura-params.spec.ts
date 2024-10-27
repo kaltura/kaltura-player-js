0af124ded2ce7d4bbed1359002ad63956411c64a
MosheMaorKaltura <moshe.maor@kaltura.com>
2024-10-27 09:24:14 +0200

    FEC-14189: lint fix in test files

--- tests/e2e/common/utils/kaltura-params.spec.ts
+++ tests/e2e/common/utils/kaltura-params.spec.ts
@@ -391,24 +391,32 @@ describe('testReferrerLogic', () => {
   });
 
   it('referrer on parent', () => {
-    sandbox.stub(window, 'parent').get(() => {return { document : {URL : 'parentRef'}}});
+    sandbox.stub(window, 'parent').get(() => {
+      return { document: { URL: 'parentRef' } };
+    });
     getReferrer().should.equal('parentRef');
   });
 
   it('no referrer on parent and backend supplied referrer', () => {
-    sandbox.stub(window, 'parent').get(() => {return { document : {URL : undefined}}});
-    sandbox.stub(window, 'originalRequestReferrer').get(() => "backendRef");
+    sandbox.stub(window, 'parent').get(() => {
+      return { document: { URL: undefined } };
+    });
+    sandbox.stub(window, 'originalRequestReferrer').get(() => 'backendRef');
     getReferrer().should.equal('backendRef');
   });
 
   it('if parent referrer contains kaltura.com and backend supplied referrer', () => {
-    sandbox.stub(window, 'parent').get(() => {return { document : {URL : 'bla.kaltura.com'}}});
-    sandbox.stub(window, 'originalRequestReferrer').get(() => "test-kaltura.com");
+    sandbox.stub(window, 'parent').get(() => {
+      return { document: { URL: 'bla.kaltura.com' } };
+    });
+    sandbox.stub(window, 'originalRequestReferrer').get(() => 'test-kaltura.com');
     getReferrer().should.equal('test-kaltura.com');
   });
 
   it('if parent referrer contains kaltura.com and backend does not supplied referrer', () => {
-    sandbox.stub(window, 'parent').get(() => {return { document : {URL : 'bla.kaltura.com'}}});
+    sandbox.stub(window, 'parent').get(() => {
+      return { document: { URL: 'bla.kaltura.com' } };
+    });
     sandbox.stub(document, 'referrer').get(() => 'localRef');
     sandbox.stub(window, 'originalRequestReferrer').get(() => undefined);
     getReferrer().should.equal('localRef');
