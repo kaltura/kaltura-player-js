5bc4e1163ebc2d7601926b40e79789ca46d8de01
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2025-04-30 11:57:58 +0300

    review fix

--- src/types/log-level.ts
+++ src/types/log-level.ts
@@ -2,5 +2,5 @@ export interface LogConfig {
   playerVersion?: boolean;
   level: string;
   handler?: (messages: any[], context: Object) => void;
-  useDebugInfo: boolean;
+  useDebugInfo?: boolean;
 }
