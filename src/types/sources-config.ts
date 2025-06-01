7bef155f647b25696929c43f20effe74d91f6dcd
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2025-06-01 12:28:27 +0300

    update config

--- src/types/sources-config.ts
+++ src/types/sources-config.ts
@@ -3,11 +3,16 @@ import { MediaSourceOptionsObject } from './media-source-options';
 import { ImageSourceOptions } from './image-player-options';
 import { ProviderMediaConfigSourcesObject } from '@playkit-js/playkit-js-providers/types';
 
+export interface FallbackSourceOption {
+  fromSource: Record<string, any>;
+  toSource: Record<string, any>;
+}
+
 export interface SourcesConfig extends Omit<ProviderMediaConfigSourcesObject, 'poster'> {
   thumbnails?: ExternalThumbnailsConfig;
   options: MediaSourceOptionsObject;
   startTime: number;
   imageSourceOptions?: ImageSourceOptions;
   poster?: string;
-  fallbackSourceOptions: any;
+  fallbackSourceOptions?: FallbackSourceOption[];
 }
