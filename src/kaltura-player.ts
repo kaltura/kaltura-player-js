810afa43d26626b7141340ac3b8d80fec3c93004
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2026-03-12 13:58:29 +0200

    fix erorr

--- src/kaltura-player.ts
+++ src/kaltura-player.ts
@@ -335,7 +335,7 @@ export class KalturaPlayer extends FakeEventTarget {
     this._thumbnailUpdatedKs = newKs;
     this._thumbnailManager?.updateThumbnailKs();
     if (sendEvent) {
-      //@ts-ignore
+      // @ts-expect-error - internal event, not typed
       this._localPlayer.dispatchEvent(new FakeEvent(CoreEventType.THUMBNAIL_KS_UPDATED, { newKs }));
     }
   }
